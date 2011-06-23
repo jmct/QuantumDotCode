ff9998f7f987c44134e1d68ebc12332ff0362c84
Jose Calderon <jose+gitt@jmct.cc>
2011-06-23 13:25:00 +0100

    Initial

--- matlab/setHamiltonian.m
+++ matlab/setHamiltonian.m
@@ -1,11 +1,14 @@
 function H = setHamiltonian(n, corners)
-	H = zeros(n);
+	H = zeros(n); %initialize empty matrix of size n
+	
+	%use determined coeefficients to fill diagonal
 	for i = 2:n-1
 		H(i,i-1) = 1;
 		H(i,i)   = 2;
 		H(i,i+1) = 1;
 	end
 	
+	%Ensure that the corners of the matrix are what we desire
 	H(1,1) = corners;
 	H(1,2) = 1;
 	H(n,n) = corners;
