a4e21c7575f431cc86aa5ffcdb30a04d1e49cb3a
Jose Calderon <jose+gitt@jmct.cc>
2011-07-12 23:38:18 +0100

    Fixed Circle Potential and Started adding sparse representations

--- matlab/circlePotential.m
+++ matlab/circlePotential.m
@@ -10,13 +10,8 @@ function V = circlePotential(n, radius)
             dist = sqrt((abs(i-center)^2)+(abs(j-center))^2);
             index = coordToIndex(i,j);
             if (dist <= radius)
-                V(index,index) = 0; %this is wrong the indeces of V
-                   %do not accurately reflect the values they should.
-                   %example: (2,1) of the sample square should be V(11,11)
-                   %but do to my stupid math it is currently re-writing
-                   %V(2,2). So the results are all wrong. 
-                   %Need to write a translator from the sample square
-                   %coordinates to proper matrix indeces. 
+                V(index,index) = 0; 
+
             else
                 V(index,index) = realmax;
             end
