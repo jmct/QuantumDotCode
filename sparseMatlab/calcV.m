1d031e7a7bb4cc1a9113758bf8ef8031b804c709
Jose Calderon <jose+gitt@jmct.cc>
2011-07-26 22:36:44 +0100

    Change the V functions to reflect new abstraction for finding points in a shape

--- sparseMatlab/calcV.m
+++ sparseMatlab/calcV.m
@@ -1,18 +1,6 @@
-function V = calcV(shapes, numShapes, n)
-	%the shapes structure is a cell array where the
-	%first dimension corresponds to the primitives
-	%as follows: 1->circle, 2->cube, 3->cylinder
-	%and 4->pyramid
-
-	%the second dimension is for each instance of 
-	%the relavent primitive. So shapes{2,4} would
-	%correspond to the 4th cube
-
-	%the numShapes vector holds the number of 
-	%instances for each shape where the position
-	%in the vector relates to the type of primitive
-	%(same order as the first dimension of the cell 
-	%array)
+function V = calcV(shapes, n)
+	%the shapes structure is a cell array containing
+	%all the defined shapes
 
 	indeces = zeros(n^3,1);
 	%indeces will be the diagonal of the V matrix
@@ -25,36 +13,21 @@ function V = calcV(shapes, numShapes, n)
 	for z = 1:n
 	    for y = 1:n
 	        for x = 1:n
-	            indx = coordToIndex3D(x,y,z,n);
-		    indeces(count) = indx;
-		    hit = 0;
-	            %go through each Sphere
-	            for i = 1:numShapes(1)
-	                if (isInSphere(x,y,z,shapes{1,i}))
-	                    hit = 1;
-			end
-		    end
-		
-		    %each Cube
-		    for i = 1:numShapes(2)
-			if (isInCube(x,y,z,shapes{2,i}))
-			    hit = 1;
-			end
-		    end
+				indx = coordToIndex3D(x,y,z,n);
+				indeces(count) = indx;
+				hit = 0;
 				
-		    %each cylinder
-		    for i = 1:numShapes(3)
-			if (isInCylinder(x,y,z,shapes{3,i}))
-			    hit = 1;
+				for i = 1:size(size(shapes,2))
+					if (isInShape(shape{i}))
+						hit = 1;
+					end
+				end
+				
+				if (hit == 0)
+					count = count + 1;
+				end
 			end
-		    end
-				
-		    if (hit == 0)
-		        count = count + 1;
-		    end
-				
 		end
-	    end
 	end
 	
 	V = sparse(indeces(1:(count-1)), indeces(1:(count-1)), realmax);
--- sparseMatlab/calcVPotential.m
+++ sparseMatlab/calcVPotential.m
@@ -1,18 +1,6 @@
-function V = calcV(shapes, numShapes, elecFieldMax, n)
-	%the shapes structure is a cell array where the
-	%first dimension corresponds to the primitives
-	%as follows: 1->circle, 2->cube, 3->cylinder
-	%and 4->pyramid
-
-	%the second dimension is for each instance of 
-	%the relavent primitive. So shapes{2,4} would
-	%correspond to the 4th cube
-
-	%the numShapes vector holds the number of 
-	%instances for each shape where the position
-	%in the vector relates to the type of primitive
-	%(same order as the first dimension of the cell 
-	%array)
+function V = calcV(shapes, elecFieldMax, n)
+	%the shapes structure is a cell array containing
+	%all the defined shapes
 
 	indeces = zeros(n^3,1);
 	%indeces will be the diagonal of the V matrix
@@ -29,32 +17,16 @@ function V = calcV(shapes, numShapes, elecFieldMax, n)
 	%the electric potential of a point at x = 1 is 0
 
 	for z = 1:n
-	    for y = 1:n
-	        for x = 1:n
+		for y = 1:n
+			for x = 1:n
 				indx = coordToIndex3D(x,y,z,n);
 				hit = 0;
 				elecPot = m*x - 1;
 				
 				
-				%go through each Sphere
-			    for i = 1:numShapes(1)
-			    	if (isInSphere(x,y,z,shapes{1,i}))
-						indeces(indx) = elecPot;
-			    		hit = 1;
-					end
-		        end
-		
-				%each Cube
-				for i = 1:numShapes(2)
-					if (isInCube(x,y,z,shapes{2,i}))
-						indeces(indx) = elecPot;
-						hit = 1;
-					end
-				end
-				
-				%each cylinder
-				for i = 1:numShapes(3)
-					if (isInCylinder(x,y,z,shapes{3,i}))
+				%go through each shape
+				for i = 1:size(shapes,2)
+					if (isInShape(shapes{i}))
 						indeces(indx) = elecPot;
 						hit = 1;
 					end
@@ -62,10 +34,9 @@ function V = calcV(shapes, numShapes, elecFieldMax, n)
 				
 				if (hit == 0)
 					indeces(indx) = realmax;
-				end
-				
+				end				
 			end
-	    end
+		end
 	end
 	
 	V = sparse(indeces, indeces, realmax);
