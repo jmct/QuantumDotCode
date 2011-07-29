89bc29dd2d921f48e0083e41bd3ac4963f110fff
Jose Calderon <jose+gitt@jmct.cc>
2011-07-29 01:46:39 +0100

    Just changing the experiment

--- sparseMatlab/cubeTest1.m
+++ sparseMatlab/cubeTest1.m
@@ -1,18 +1,18 @@
-cube = [2,10,10,10,30,30,30];
+cube = [2,3,3,3,19,19,19];
 shapes = {cube};
-K = setupK3(50);
+K = setupK3(25);
 
-fileID = fopen('/usr/jmc512/QuantumDotCode/sparseMatlab/cubeTest1.csv', 'a');
+fileID = fopen('/usr/jmc512/QuantumDotCode/sparseMatlab/cubeTest3.csv', 'a');
 temp = 0;
 
 for i = 1:100
-    V = calcVPotential(shapes,i*100, 50, 1000);
+    V = calcVPotential(shapes,i*10, 25, 1000);
     H = K + V;
     tic
     eigsAns = eigs(H,2,'SM');
     toc
     temp = eigsAns(1) - eigsAns(2);
-    fprintf(fileID,'%d,%f\n', i*100, temp);
+    fprintf(fileID,'%d,%f\n', i*10, temp);
     i
 end
 
