f9f6300098cc773536e1caf1dc7d610228717b58
Jose Calderon <jose+gitt@jmct.cc>
2011-08-03 22:06:07 +0100

    Updated .gitignore for some random test files I needed

--- sparseMatlab/KCheck.m
+++ sparseMatlab/KCheck.m
@@ -1,12 +1,13 @@
 L = 1;
 frac = .8;
-results = zeros(1,10);
-for i = 1:10
+results = zeros(1,15);
+for i = 1:15
 N = ((5+(i-1))*5)+1;
 M = 1+frac*(N - 1);
 H = (((M-1)/(frac*L))^2) * setupK3(M);
 dif = eigs(H, 2, 'SM');
 results(:,i) = dif(1)  - dif(2);
+i
 end
 
-plot(1:10, results);
+plot(1:15, results);
