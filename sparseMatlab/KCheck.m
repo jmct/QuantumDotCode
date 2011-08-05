83dd0b71a0e06bb5249144c9ed955ffc3e436a7c
Jose Calderon <jose+gitt@jmct.cc>
2011-08-05 15:58:22 +0100

    Changed KCheck to plot proper X-axis

--- sparseMatlab/KCheck.m
+++ sparseMatlab/KCheck.m
@@ -1,13 +1,23 @@
 L = 1;
 frac = .8;
-results = zeros(1,15);
-for i = 1:15
-N = ((5+(i-1))*5)+1;
-M = 1+frac*(N - 1);
+
+iterations = 15;
+results = zeros(1,iterations);
+N = zeros(1, iterations);
+
+for i = 1:iterations
+    N(i) = ((5+(i-1))*5)+1;
+    
+end
+for i = 1:iterations
+M = 1+frac*(N(i) - 1);
 H = (((M-1)/(frac*L))^2) * setupK3(M);
 dif = eigs(H, 2, 'SM');
 results(:,i) = dif(1)  - dif(2);
 i
 end
 
-plot(1:15, results);
+figure, close
+
+
+plot(N, results);
