217b2377484ee58f14ecf046df4388e6ed507644
Jose Calderon <jose+gitt@jmct.cc>
2011-09-13 21:39:02 +0100

    Added new KCheck for many eigenvalues

---
+++ sparseMatlab/KCheck2.m
@@ -0,0 +1,22 @@
+L = 1;
+frac = .8;
+
+iterations = 15;
+results = zeros(1,iterations);
+N = zeros(1, iterations);
+
+for i = 1:iterations
+    N(i) = ((5+(i-1))*5)+1;
+    
+end
+for i = 1:iterations
+M = 1+frac*(N(i) - 1);
+H = (((M-1)/(frac*L))^2) * setupK3(M);
+dif = eigs(H, 2, 'SM');
+results(:,i) = dif'
+end
+
+FileID = fopen('ManyEigen.csv', 'w');
+fprintf(FileID, '%d,%f\n', [N;results]);
+
+fclose(FileID);
